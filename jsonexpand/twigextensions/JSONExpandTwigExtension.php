ce01c2f9e7cf6061c6d6617788f15d6927e7f3e1
Keith Mancuso <keithmancuso@gmail.com>
2014-05-17 19:18:39 -0400

    Cleaned up whitespace

--- jsonexpand/twigextensions/JSONExpandTwigExtension.php
+++ jsonexpand/twigextensions/JSONExpandTwigExtension.php
@@ -1,55 +1,43 @@
 <?php
 namespace Craft;
 
-class JSONExpandTwigExtension extends \Twig_Extension
-{
+class JSONExpandTwigExtension extends \Twig_Extension {
 
-	public function getName()
-	{
+	public function getName() {
 		return Craft::t('JSON');
 	}
 
-	public function getFilters()
-	{
+	public function getFilters() {
 		return array(
 			'json_expand' => new \Twig_Filter_Method($this, 'jsonExpandFilter')
 		);
 	}
 
-	public function jsonExpandFilter($content)
-	{
+	public function jsonExpandFilter($content) {
 
 		if (is_array($content)) {
 
 			$expandedContent = array();
+
 			foreach ($content as $entry) {
-
 				$expandedContent[] = $this->getEntryJson($entry);
-
 			}
 
 		} elseif (is_object($content)) {
-
 			$expandedContent = $this->getEntryJson($content);
-
 		} else {
-
 			$expandedContent = null;
-
 		}
 
 		return JsonHelper::encode($expandedContent);
 
 	}
 
-	private function getEntryJson(EntryModel $entry)
-	{
+	private function getEntryJson(EntryModel $entry) {
 	    $entryData = array();
-
 			$entryData['title'] = $entry->title;
 
-	    foreach ($entry->getType()->getFieldLayout()->getFields() as $field)
-	    {
+	    foreach ($entry->getType()->getFieldLayout()->getFields() as $field) {
 
 				$field = $field->getField();
         $handle = $field->handle;
@@ -60,20 +48,18 @@ class JSONExpandTwigExtension extends \Twig_Extension
 						$value = array_merge((array)$value);
 				}
 
-				// TODO: Need to add conditionals for Matrix, dropdowns
-
 				// check if its a relational field type
 				if ( $field['type'] == 'Categories' || $field['type'] == 'Users' || $field['type'] == 'Assets' || $field['type'] == 'Entries' || $field['type'] == 'Matrix'  ) {
 
 					// for each related element
-					foreach ($value as $relatedElement)
-					{
+					foreach ($value as $relatedElement) {
+
 						$relatedArray = array();
 
 						// gets all the default attributes from the element
 						$relatedArray = $relatedElement->getAttributes();
 
-						// setup for further customizations
+						// setup switch for fieldtype specific further customizations
 						switch ($field['type']) {
 							case 'Assets':
 								$relatedArray['url'] = $relatedElement->url;
@@ -82,20 +68,17 @@ class JSONExpandTwigExtension extends \Twig_Extension
 						 }
 
 						// get all the custom fields
-						foreach ($relatedElement->getFieldLayout()->getFields() as $subField)
-						{
+						foreach ($relatedElement->getFieldLayout()->getFields() as $subField) {
 
 							$subField = $subField->getField();
 							$subHandle = $subField->handle;
 							$subValue = $relatedElement->$subHandle;
 
-							if ($subValue instanceof \ArrayObject)
-							{
+							if ($subValue instanceof \ArrayObject) {
 									$subValue = array_merge((array)$subValue);
 							}
 
 							$relatedArray[$subHandle] = $subValue;
-
 
 						}
 						// add the item to the fields array
@@ -113,17 +96,9 @@ class JSONExpandTwigExtension extends \Twig_Extension
 	    return $entryData;
 	}
 
-
 	// not in use now but seemed like a good idea to abstract out?
 	private function getFieldData($field) {
-
-		$fieldObject;
-		$field = $field->getField();
-		$handle = $field->handle;
-		$value = $entry->$handle;
-		$fieldObject->$handle = $value;
-		return $fieldObject;
-
+		return $field;
 	}
 
 }
