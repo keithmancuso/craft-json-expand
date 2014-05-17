3799a9ac1e36a049e2174e541f79711d338ebe30
Keith Mancuso <keithmancuso@gmail.com>
2014-05-17 17:29:12 -0400

    Got basic relational field types outputting

--- jsonexpand/twigextensions/JSONExpandTwigExtension.php
+++ jsonexpand/twigextensions/JSONExpandTwigExtension.php
@@ -22,9 +22,9 @@ class JSONExpandTwigExtension extends \Twig_Extension
 		if (is_array($content)) {
 
 			$expandedContent = array();
-			foreach ($content as $key => $value) {
+			foreach ($content as $entry) {
 
-				$expandedContent[] = $this->getEntryJson($value);
+				$expandedContent[] = $this->getEntryJson($entry);
 
 			}
 
@@ -42,62 +42,101 @@ class JSONExpandTwigExtension extends \Twig_Extension
 
 	}
 
-	public function getEntryJson(EntryModel $entry)
+	private function getEntryJson(EntryModel $entry)
 	{
 	    $entryData = array();
 
+			$entryData['title'] = $entry->title;
+
 	    foreach ($entry->getType()->getFieldLayout()->getFields() as $field)
 	    {
-	        $field = $field->getField();
-	        $handle = $field->handle;
-	        $value = $entry->$handle;
 
-	        if ($field instanceof BaseElementFieldType)
-	        {
-	            $entryData[$handle] = array();
+				$field = $field->getField();
+        $handle = $field->handle;
+        $value = $entry->$handle;
 
-	            foreach ($value as $relElement)
-	            {
-	                $entryData[$handle][] = array(
-	                    'id' => $relElement->id,
-	                    'label' => (string) $relElement
-	                );
-	            }
-	        }
-	        else if ($field instanceof MatrixFieldType)
-	        {
-	            $entryData[$handle] = array();
+				// TODO: Need to add conditionals for Matrix, dropdowns
 
-	            foreach ($value as $block)
-	            {
-	                $entryData[$handle][] = array(
-	                    // ...
-	                );
-	            }
-	        }
-	        else
-	        {
-	            // Deal with Checkboxes and Multi-select fields
-	            if ($value instanceof \ArrayObject)
-	            {
-	                $value = array_merge($value);
-	            }
+				// check if its a relational field type
+				if ( $field['type'] == 'Categories' || $field['type'] == 'Users' || $field['type'] == 'Assets' || $field['type'] == 'Entries' || $field['type'] == 'Matrix'  ) {
 
-	            if (is_array($value))
-	            {
-	                $entryData[$handle] = $value;
-	            }
-	            else
-	            {
-	                // Let's just force anything else to a string, in case it's something like a SingleOptionFieldData class
-	                //$entryData[$handle] = (string) $value;
-	            }
+					// for each related element
+					foreach ($value as $key=> $relatedElement)
+					{
 
-	            $entryData[$handle] = $value;
-	        }
+						$relatedArray = array (
+							'id' => $relatedElement->id,
+							'label' => (string) $relatedElement
+						);
+
+						// add additional standard fields for different types
+						if ($field['type'] == 'Catgories') {
+							$relatedArray['slug'] = $relatedElement->slug;
+						} else if ($field['type'] == 'Assets') {
+							$relatedArray['url'] = $relatedElement->url;
+							$relatedArray['filename'] = $relatedElement->filename;
+						} else if ($field['type'] == 'Users') {
+							$relatedArray['firstName'] = $relatedElement->firstName;
+							$relatedArray['lastName'] = $relatedElement->lastName;
+						}
+
+						// TODO: get asset transforms for each asset
+
+						foreach ($relatedElement->getFieldLayout()->getFields() as $subField)
+						{
+
+							$subField = $subField->getField();
+							$subHandle = $subField->handle;
+							$subValue = $relatedElement->$subHandle;
+
+							//$categoryObject[$subHandle]['field'] =  $subField;
+							//echo 'GOT FIELD'.$subHandle;
+							$relatedArray[$subHandle] = $subValue;
+							//var_dump($categoryObject);
+
+						}
+
+						$entryData[$handle][] = $relatedArray;
+					}
+
+				} else {
+
+					// Deal with Checkboxes and Multi-select fields
+					// these are pulled over from brandons code but dont seem to work?
+					if ($value instanceof \ArrayObject)
+					{
+					    $value = array_merge($value);
+					}
+
+					if (is_array($value))
+					{
+					    $entryData[$handle] = $value;
+					} else {
+						// out field info for debugging
+						//$entryData[$handle] = $field;
+						$entryData[$handle] = $value;
+
+					}
+
+				}
+
 	    }
 
 	    return $entryData;
+	}
+
+
+	// not in use now but seemed like a good idea to abstract out?
+	private function getFieldData($field) {
+
+
+		$fieldObject;
+		$field = $field->getField();
+		$handle = $field->handle;
+		$value = $entry->$handle;
+		$fieldObject->$handle = $value;
+		return $fieldObject;
+
 	}
 
 }
