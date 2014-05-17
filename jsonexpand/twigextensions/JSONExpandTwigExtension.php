4673c34628e954af30d2b6cb1d05e1f0429c69a4
Keith Mancuso <keithmancuso@gmail.com>
2014-05-17 19:23:20 -0400

    More whitespace uglyness

--- jsonexpand/twigextensions/JSONExpandTwigExtension.php
+++ jsonexpand/twigextensions/JSONExpandTwigExtension.php
@@ -34,66 +34,64 @@ class JSONExpandTwigExtension extends \Twig_Extension {
 	}
 
 	private function getEntryJson(EntryModel $entry) {
-	    $entryData = array();
-			$entryData['title'] = $entry->title;
+		$entryData = array();
+		$entryData['title'] = $entry->title;
 
-	    foreach ($entry->getType()->getFieldLayout()->getFields() as $field) {
+		foreach ($entry->getType()->getFieldLayout()->getFields() as $field) {
 
-				$field = $field->getField();
-        $handle = $field->handle;
-        $value = $entry->$handle;
+			$field = $field->getField();
+			$handle = $field->handle;
+			$value = $entry->$handle;
 
-				if ($value instanceof \ArrayObject)
-				{
-						$value = array_merge((array)$value);
-				}
+			if ($value instanceof \ArrayObject) {
+				$value = array_merge((array)$value);
+			}
 
-				// check if its a relational field type
-				if ( $field['type'] == 'Categories' || $field['type'] == 'Users' || $field['type'] == 'Assets' || $field['type'] == 'Entries' || $field['type'] == 'Matrix'  ) {
+			// check if its a relational field type
+			if ( $field['type'] == 'Categories' || $field['type'] == 'Users' || $field['type'] == 'Assets' || $field['type'] == 'Entries' || $field['type'] == 'Matrix'  ) {
 
-					// for each related element
-					foreach ($value as $relatedElement) {
+				// for each related element
+				foreach ($value as $relatedElement) {
 
-						$relatedArray = array();
+					$relatedArray = array();
 
-						// gets all the default attributes from the element
-						$relatedArray = $relatedElement->getAttributes();
+					// gets all the default attributes from the element
+					$relatedArray = $relatedElement->getAttributes();
 
-						// setup switch for fieldtype specific further customizations
-						switch ($field['type']) {
-							case 'Assets':
-								$relatedArray['url'] = $relatedElement->url;
-								break;
+					// setup switch for fieldtype specific further customizations
+					switch ($field['type']) {
+						case 'Assets':
+							$relatedArray['url'] = $relatedElement->url;
+							break;
 
-						 }
+					 }
 
-						// get all the custom fields
-						foreach ($relatedElement->getFieldLayout()->getFields() as $subField) {
+					// get all the custom fields
+					foreach ($relatedElement->getFieldLayout()->getFields() as $subField) {
 
-							$subField = $subField->getField();
-							$subHandle = $subField->handle;
-							$subValue = $relatedElement->$subHandle;
+						$subField = $subField->getField();
+						$subHandle = $subField->handle;
+						$subValue = $relatedElement->$subHandle;
 
-							if ($subValue instanceof \ArrayObject) {
-									$subValue = array_merge((array)$subValue);
-							}
-
-							$relatedArray[$subHandle] = $subValue;
-
+						if ($subValue instanceof \ArrayObject) {
+								$subValue = array_merge((array)$subValue);
 						}
-						// add the item to the fields array
-						$entryData[$handle][] = $relatedArray;
+
+						$relatedArray[$subHandle] = $subValue;
+
 					}
-
-
-				} else {
-					// just set the field value to the field
-					$entryData[$handle] = $value;
+					// add the item to the fields array
+					$entryData[$handle][] = $relatedArray;
 				}
 
-	    }
 
-	    return $entryData;
+			} else {
+				// just set the field value to the field
+				$entryData[$handle] = $value;
+			}
+		}
+
+		return $entryData;
 	}
 
 	// not in use now but seemed like a good idea to abstract out?
